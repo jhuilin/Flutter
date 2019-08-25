a92d8f07f37d173edd09899b7c118d01181ac160
jianhui <jianhuilin1124@gmail.com>
2019-08-25 13:42:37 -0400

    add provider

--- dicee-flutter/lib/main.dart
+++ dicee-flutter/lib/main.dart
@@ -18,6 +18,31 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              flex: 1,                //make it flexible
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: FlatButton(
+                  child: Image.asset('images/dice1.png'),
+                  onPressed: () => Image.asset('images/dice2.png'),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child:Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: FlatButton(
+                  child: Image.asset('images/dice1.png'),
+                  onPressed: () =>null,
+                  ),
+              ),
+            ),
+          ],
+      ),
+    );
   }
 }
