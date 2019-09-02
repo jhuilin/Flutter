89574ab887b3892092e6960ac78b75dac45bf1f6
jianhui <jianhuilin1124@gmail.com>
2019-09-01 22:33:06 -0400

    finished dice

--- dicee-flutter/lib/main.dart
+++ dicee-flutter/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,7 +17,27 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+  void changeLeft(){
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+  void changeRight(){
+  setState(() {
+    rightDiceNumber = Random().nextInt(6) + 1;
+  });
+}
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -26,8 +48,10 @@ class DicePage extends StatelessWidget {
               child: Padding(
                 padding: const EdgeInsets.all(16.0),
                 child: FlatButton(
-                  child: Image.asset('images/dice1.png'),
-                  onPressed: () => Image.asset('images/dice2.png'),
+                  onPressed: (){
+                    changeLeft();
+                  },
+                  child: Image.asset('images/dice$leftDiceNumber.png'),
                 ),
               ),
             ),
@@ -36,8 +60,10 @@ class DicePage extends StatelessWidget {
               child:Padding(
                 padding: const EdgeInsets.all(16.0),
                 child: FlatButton(
-                  child: Image.asset('images/dice1.png'),
-                  onPressed: () =>null,
+                  onPressed: (){
+                    changeRight();
+                  },
+                  child: Image.asset('images/dice$rightDiceNumber.png'),
                   ),
               ),
             ),
